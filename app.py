f38d2ae79112a19eda66569feb900f5f79ed5e79
Dao <dooddao51246@gmail.com>
2026-09-14 11:54:00 +0700

    เพิ่มชื่อ

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.markdown(
     """
     | คนที่ | ชื่อ-นามสกุล | หน้าที่หลัก |
     |---|---|---|
-    | 1 | (ใส่ชื่อ) | Temperature Monitoring |
+    | 1 | นางสาว ดุจดาว ลุงพงษ์| Temperature Monitoring |
     | 2 | นายศักดิ์สกุล อนุภาพ | Humidity Monitoring |
     | 3 | (ใส่ชื่อ) | Power Monitoring |
     | 4 | (ใส่ชื่อ) | Safety Alarm |
